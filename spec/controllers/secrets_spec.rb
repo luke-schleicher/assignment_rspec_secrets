516105e773d4b5873c2907f861ee8eb8ff5fb1ac
luke-schleicher <luke.j.schleicher@gmail.com>
2016-12-14 11:17:31 -0600

    started writing tests

---
+++ spec/controllers/secrets_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe SecretsController do
+
+  
+
+end
---
+++ spec/controllers/users_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe UsersController do
+
+  context 'get#create' do
+
+    it 'creates a new user'
+
+
+  end
+
+end
